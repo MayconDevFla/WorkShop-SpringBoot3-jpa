d676b5abe0e90f804b114213558ff79934afff8f
Maycon Douglas <Maicon_Stannley@hotmail.com>
2023-03-28 00:21:21 -0300

    Service layer, component registration

---
+++ src/main/java/com/teste/demo/services/UserService.java
@@ -0,0 +1,25 @@
+package com.teste.demo.services;
+
+import com.teste.demo.repositories.UserRepository;
+import com.teste.demo.entities.User;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
